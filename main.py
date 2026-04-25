dd833f7712f924c2dc4032da82bc14b6e009796d
belangerruiz-source <belangerruiz@gmail.com>
2026-04-25 14:03:48 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ import os
 TOKEN = os.getenv("TOKEN")
 
 def precio(symbol):
-    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
-    return float(requests.get(url).json()["price"])
+    try:
+        url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
+        r = requests.get(url, timeout=8)
+        data = r.json()
+        return float(data["price"])
+    except:
+        return "Error"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot activo. Usa /scan")
 
 async def scan(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Escaneando mercado...")
+
     trx = precio("TRXUSDT")
     ada = precio("ADAUSDT")
     doge = precio("DOGEUSDT")
@@ -29,7 +36,9 @@ Versión 1 activa
     await update.message.reply_text(texto)
 
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("scan", scan))
 
+print("Bot iniciado")
 app.run_polling()
